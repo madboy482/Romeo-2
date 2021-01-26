87b4001c78da3e84723e7a89b9debaa745dad024
madboy482 <73926361+madboy482@users.noreply.github.com>
2021-01-26 12:29:46 +0530

    Update fun.py

--- Romeo/modules/fun.py
+++ Romeo/modules/fun.py
@@ -6,7 +6,7 @@ import Romeo.modules.fun_strings as fun_strings
 from Romeo import dispatcher
 from Romeo.modules.disable import DisableAbleCommandHandler
 from Romeo.modules.helper_funcs.chat_status import is_user_admin
-from SRomeo.modules.helper_funcs.extraction import extract_user
+from Romeo.modules.helper_funcs.extraction import extract_user
 from telegram import ChatPermissions, ParseMode, Update
 from telegram.error import BadRequest
 from telegram.ext import CallbackContext, run_async
